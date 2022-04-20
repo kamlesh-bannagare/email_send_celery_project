978774a95a4473f2843214d1726bb4508072038d
kamlesh bannagare <kamleshbannagare10@gmail.com>
2022-04-20 14:59:09 +0530

    first commit

---
+++ celery_with_django/django_celery_project/celery.py
@@ -0,0 +1,41 @@
+from __future__ import absolute_import, unicode_literals
+import os
+
+from celery import Celery
+from django.conf import settings
+
+# crontab
+from celery.schedules import crontab
+from django_celery_beat.models import PeriodicTask
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_celery_project.settings')
+
+app = Celery('django_celery_project')
+app.conf.enable_utc = False
+
+app.conf.update(timezone = 'Asia/Kolkata')
+
+app.config_from_object(settings, namespace='CELERY')
+
+# CELERY BEAT SETTINGS
+app.conf.beat_schedule = {
+    'send-mail-every-day-at-8': {
+        'task': 'send_mail_app.tasks.send_mail_func',
+        'schedule': crontab(hour=13, minute=59, day_of_month=20, month_of_year = 4),
+        #'args': (2,)
+    }
+    
+}
+
+
+
+
+
+# Celery Schedules - https://docs.celeryproject.org/en/stable/reference/celery.schedules.html
+
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
---
+++ celery_with_django/send_mail_app/tasks.py
@@ -0,0 +1,22 @@
+from django.contrib.auth import get_user_model
+
+from celery import shared_task
+from django.core.mail import send_mail
+from django_celery_project import settings
+
+@shared_task(bind=True)
+def send_mail_func(self):
+    users = get_user_model().objects.all()
+    for user in users:
+        mail_subject = "Hi! Celery Testing"
+        message = "hi this message is for testing purpose. ok, Have a good day!"
+        to_email = user.email
+        send_mail(
+            subject = mail_subject,
+            message = message,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[to_email],
+            fail_silently=True,
+        )
+        
+    return "done"
